e2b1fc1b843bd3b9a5464fd326ddbe6094b2e193
Mitra Khorshidi <mitkhorshidi@gmail.com>
2022-07-06 16:58:32 +0430

    error

--- components/reservation.js
+++ components/reservation.js
@@ -21,12 +21,12 @@ const Reservation = () =>{
                     <p>Number of People</p>
                     <input type="number" className="border-1 border-slate-400 rounded-sm focus:border-black focus:ring-0 h-10"/>
                     <p>Table Number</p>
-                    <input type="number" className="border-1 border-slate-400 rounded-sm focus:border-black focus:ring-0 h-10"/>
+                    <input type={"number"} className="border-1 border-slate-400 rounded-sm focus:border-black focus:ring-0 h-10"/>
                     <input type="submit" value="Book" className="bg-teal-300 hover:bg-teal-400 px-4 py-1 font-medium rounded-md shadow-sm self-start mt-5"/>
                 </div>
                 <div className="flex flex-col gap-y-1">
                     <p>Phone Number</p>
-                    <input type="number" className="border-1 border-slate-400 rounded-sm focus:border-black focus:ring-0 h-10"/>
+                    <input type="tel" className="border-1 border-slate-400 rounded-sm focus:border-black focus:ring-0 h-10"/>
                     <p>Date</p>
                     <ReactDatePicker showTimeSelect dateFormat="MMMM d, yyyy h:mmaa" selected={date} onChange={(date) => setdate(date)} className="border-1 border-slate-400 rounded-sm focus:border-black focus:ring-0 h-10"/>
                     <p>Are you a Host ?</p>
