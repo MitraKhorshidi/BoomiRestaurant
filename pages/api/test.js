c8b258b91961bc08bb1f9ccef6680be12a536ac9
Mitra Khorshidi <mitkhorshidi@gmail.com>
2022-08-21 16:09:04 +0430

    test

---
+++ pages/api/test.js
@@ -0,0 +1,9 @@
+import { FoodRepository } from "../../data/repository";
+
+export default async function handler(req, res) {
+  try {
+    res.status(200).json({ data:await FoodRepository.list() });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
